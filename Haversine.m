d3a2bd2fd94a5bafa2556fd7c201dc383da4f792
heymanuuu <158561644+heymanuuu@users.noreply.github.com>
2024-08-09 17:39:32 +0800

    Create Haversine.m

---
+++ Haversine.m
@@ -0,0 +1,13 @@
+% Haversine formula to calculate the distance between two points
+function distance = haversine(lat1, lon1, lat2, lon2)
+    R = 6371; %Earth's radius, in kilometers
+    lat1 = deg2rad(lat1);
+    lon1 = deg2rad(lon1);
+    lat2 = deg2rad(lat2);
+    lon2 = deg2rad(lon2);
+    dlat = lat2 - lat1;
+    dlon = lon2 - lon1;
+    a = sin(dlat/2)^2 + cos(lat1) * cos(lat2) * sin(dlon/2)^2;
+    c = 2 * atan2(sqrt(a), sqrt(1-a));
+    distance = R * c;
+end
